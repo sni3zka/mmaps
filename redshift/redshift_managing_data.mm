63c6457c8447a7ae6f32c998028e4b829c728c28
karolinak <karolinakajak@timeout.com>
2014-01-08 15:17:23 +0000

    next chapter

---
+++ redshift/redshift_managing_data.mm
@@ -0,0 +1,10 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1389194151153" ID="ID_974655068" MODIFIED="1389194178281" TEXT="RS managing">
+<node CREATED="1389194194807" ID="ID_669569619" MODIFIED="1389194196917" POSITION="right" TEXT="how">
+<node CREATED="1389194196919" ID="ID_320436006" MODIFIED="1389194205896" TEXT="cmd interface"/>
+<node CREATED="1389194207644" ID="ID_1213740784" MODIFIED="1389194210544" TEXT="API calls"/>
+<node CREATED="1389194210963" ID="ID_744519949" MODIFIED="1389194223672" TEXT="RS managment console"/>
+</node>
+</node>
+</map>
